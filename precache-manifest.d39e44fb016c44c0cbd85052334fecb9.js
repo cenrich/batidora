3006d4d50b3a7b0f64bc9768ecf4574d32daf8e1
cenrich <ceci_enrich@hotmail.com>
2019-11-08 07:18:38 -0300

    Updates

---
+++ precache-manifest.d39e44fb016c44c0cbd85052334fecb9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "938273eb49a2c65100e3000a4ecc225c",
+    "url": "/batidora/index.html"
+  },
+  {
+    "revision": "2e89e9e4283b226a98f8",
+    "url": "/batidora/static/css/2.764ccc25.chunk.css"
+  },
+  {
+    "revision": "d18a9ad45d5be07347dc",
+    "url": "/batidora/static/css/main.cc1d42a6.chunk.css"
+  },
+  {
+    "revision": "2e89e9e4283b226a98f8",
+    "url": "/batidora/static/js/2.9fd64a26.chunk.js"
+  },
+  {
+    "revision": "d18a9ad45d5be07347dc",
+    "url": "/batidora/static/js/main.e9eecf27.chunk.js"
+  },
+  {
+    "revision": "416f872221bbb47ade9c",
+    "url": "/batidora/static/js/runtime-main.ee54b444.js"
+  }
+]);
